f446a2426256340980cf81ea0c52c9f68894dae1
Terry <uestc_lxh@163.com>
2017-09-17 13:24:20 +0800

    update jenkins

--- test/FreeGofPYLib.py
+++ test/FreeGofPYLib.py
@@ -4,6 +4,7 @@ import os
 
 def robot_test_echoName(jar_path, name):
     jar_path=os.path.abspath(jar_path)
+    print jar_path
     jpype.startJVM(jpype.getDefaultJVMPath(), "-ea", "-Djava.class.path=%s" % jar_path)
     test = jpype.JClass('TestJenkins')
     t = test()
