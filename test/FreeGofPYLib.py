ba270e39c44a6df7780d44772a0c3f767e29d3a7
Terry <uestc_lxh@163.com>
2017-09-17 11:27:49 +0800

    add robot test

---
+++ test/FreeGofPYLib.py
@@ -0,0 +1,14 @@
+#-*-coding:utf-8 -*-
+import jpype
+import os
+
+def robot_test_echoName(jar_path, name):
+    jar_path=os.path.abspath(jar_path)
+    jpype.startJVM(jpype.getDefaultJVMPath(), "-ea", "-Djava.class.path=%s" % jar_path)
+    test = jpype.JClass('TestJenkins')
+    t = test()
+    res = t.echoName(name)
+    jpype.shutdownJVM()
+    return res
+
+# print robot_test_echoName("F:\PythonFiles\CommonTest\jenkins-tester.jar", "Terry")
